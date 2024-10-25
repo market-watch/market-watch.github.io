bcda670dd2caf1e9c035a38fd818f1d031600b00
market-watch <ravirajkiana@gmail.com>
2024-10-25 17:54:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -646,14 +646,14 @@ if (window.innerWidth <= 768) {
     }
 }
 
-
-// Variables to store clicked candles
+        
+        // Variables to store clicked candles
         let firstCandle = null;
         let secondCandle = null;
-
+        
         // Click event listener with direct access to OHLC values
-        var myPlot = document.getElementById('plot');
-        myPlot.on('plotly_click', function(eventData) {
+        var myPlot1 = document.getElementById('plot');
+        myPlot1.on('plotly_click', function(eventData) {
             if (!firstCandle) {
                 firstCandle = eventData.points[0];
             } else {
@@ -664,9 +664,9 @@ if (window.innerWidth <= 768) {
                 const secondLow = secondCandle.data.low[secondCandle.pointIndex];
                 const firstHigh = firstCandle.data.high[firstCandle.pointIndex];
                 const secondHigh = secondCandle.data.high[secondCandle.pointIndex];
-            
-                let percentageChange;
-            
+        
+                let percentageChange = null;
+        
                 // Check if first candle's low is greater than the second candle's low
                 if (firstLow > secondLow) {
                     percentageChange = Math.abs(((firstLow - secondLow) / firstLow) * 100);
@@ -675,21 +675,23 @@ if (window.innerWidth <= 768) {
                 else if (firstHigh < secondHigh) {
                     percentageChange = Math.abs(((secondHigh - firstHigh) / firstHigh) * 100);
                 }
-            
-                // Return the result rounded to 2 decimal points, or null if neither condition was met
-                return percentageChange ? +percentageChange.toFixed(2) : null;
+        
+                // Round the percentage change to 2 decimal points, or set to null if neither condition was met
+                percentageChange = percentageChange ? +percentageChange.toFixed(2) : null;
         
                 // Calculate number of data points between two candles
                 const numOfPoints = Math.abs(firstCandle.pointIndex - secondCandle.pointIndex);
-                document.getElementById("box_can").innerText = `${numOfPoints}`;
         
-                document.getElementById("box_pro").innerText = `${percentageChange}%`;
+                // Update the UI with calculated values
+                document.getElementById("box_can").innerText = `${numOfPoints}`;
+                document.getElementById("box_pro").innerText = `${percentageChange !== null ? percentageChange + '%' : 'N/A'}`;
         
                 // Reset for the next set of clicks
                 firstCandle = null;
                 secondCandle = null;
             }
         });
+
 
 // New function to load symbols from symbols.json
 function loadSymbols() {
